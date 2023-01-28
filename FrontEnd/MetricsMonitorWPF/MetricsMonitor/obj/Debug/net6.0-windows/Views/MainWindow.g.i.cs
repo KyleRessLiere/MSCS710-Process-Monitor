de54441f1076da892dcd10ed4461c584be91c253
salcosser <samalcosser@gmail.com>
2023-01-28 13:35:24 -0500

    MM-2 | removing bad files

--- FrontEnd/MetricsMonitorWPF/MetricsMonitor/obj/Debug/net6.0-windows/Views/MainWindow.g.i.cs
+++ FrontEnd/MetricsMonitorWPF/MetricsMonitor/obj/Debug/net6.0-windows/Views/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace MetricsMonitor.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MetricsMonitor;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MetricsMonitor;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
