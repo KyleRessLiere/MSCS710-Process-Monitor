478196dd39f28b65ed6e889b6df993c45390b6c9
KyleRessLiere <kressliere@gmail.com>
2023-02-09 11:24:51 -0500

    insert_disk

--- backend/db/service.py
+++ backend/db/service.py
@@ -52,13 +52,13 @@ def insert_disk(conn, disk):
     """
     Log a disk info to disk table
     :param conn:
-    :param poll:
-    :return: poll id
+    :param disk:
+    :return: disk id
     """
-    sql = ''' INSERT INTO polls(poll_rate,operating_system,operating_system_version,time)
-              VALUES(?,?,?,?) '''
+    sql = ''' INSERT INTO disks(poll_id, disk_total, disk_used, disk_free, disk_percentage)
+              VALUES(?,?,?,?,?) '''
     cur = conn.cursor()
-    cur.execute(sql, poll)
+    cur.execute(sql, disk)
     conn.commit()
     return cur.lastrowid
 
@@ -92,8 +92,11 @@ def main_poll(poll,polling_rate):
     INSERT disk
     """
     disk = poll["disk"]
+    
+    disk_data = (poll_id,disk["disk_total"],disk["disk_used"],disk["disk_free"],disk["disk_percent"])
+    #insert_disk(conn, disk_data)
 
-
+    
     """
     INSERT process
     """
@@ -108,3 +111,66 @@ def main_poll(poll,polling_rate):
 
     """   
 
+poll = {
+  "process": [
+    {
+      "pid": 98895,
+      "process_name": "trustd",
+      "status": "running",
+      "cpu_percent": "0.03",
+      "num_thread": 2,
+      "memory_mb": "13.771"
+    },
+    {
+      "pid": 98895,
+      "process_name": "docker",
+      "status": "running",
+      "cpu_percent": "0.00",
+      "num_thread": 14,
+      "memory_mb": "8.012"
+    }
+  ],
+  "disk": {
+    "disk_total": 1000240963584,
+    "disk_used": 11682930688,
+    "disk_free": 363153088512,
+    "disk_percent": 3.1
+  },
+  "network": [
+    {
+      "network": "lo0",
+      "status": "Up",
+      "speed": 0
+    },
+    {
+      "network": "gif0",
+      "status": "Down",
+      "speed": 0
+    },
+    {
+      "network": "utun5",
+      "status": "Up",
+      "speed": 0
+    },
+    {
+      "network": "utun6",
+      "status": "Up",
+      "speed": 0
+    }
+  ],
+  "memory": {
+    "total_memory": "17.180",
+    "used_memory": "10.114",
+    "available": "6.198",
+    "percentage": 63.9
+  },
+  "battery": {
+    "battery_percent": 78,
+    "estimated_battery_seconds_remaining": 14760
+  },
+  "cpuStats": {
+    "cpuSumPercentage": 16.0,
+    "cpuPercentageByCore": [51.0, 12.0, 46.0, 9.0, 44.0, 10.0, 35.0, 10.1]
+  }
+}
+#main_poll(poll,1)
