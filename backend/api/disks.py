7d5284841f94c7e40ba0915cd2b97c8d8156cb9f
Caleb-Rogers <rogerscaleb2019@gmail.com>
2023-02-23 15:22:07 -0500

    Disks dict obj

--- backend/api/disks.py
+++ backend/api/disks.py
@@ -4,19 +4,29 @@ import sqlite3
 from sqlite3 import Error
 
 def get_disks():
-    disks = []
+    disk_list = []
     try:
         db_file = r"./db/MMM-SQLite.db"
         conn = sqlite3.connect(db_file)
         cur = conn.cursor()
         res = cur.execute("SELECT * FROM disks")
         disks = res.fetchall()
+        for i in disks:
+            disk = {
+                "disk_id": i[0],
+                "poll_id": i[1],
+                "disk_total": i[2],
+                "disk_used": i[3],
+                "disk_free": i[4],
+                "disk_percentage": i[5]
+            }
+            disk_list.append(disk)
     except Error as e:
         print(e)
     finally:
         if conn:
             conn.close()
-    return disks
+    return disk_list
 
 def get_disk_by_id(disk_id):
     disk = {}
