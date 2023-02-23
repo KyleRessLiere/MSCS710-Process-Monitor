52525261f60d13ccb1a91b4ad61d9c3e06530c41
Caleb-Rogers <rogerscaleb2019@gmail.com>
2023-02-23 15:11:03 -0500

    Networks dict obj
    Also, updated API route to plural

--- backend/api/network.py
+++ backend/api/network.py
@@ -4,19 +4,28 @@ import sqlite3
 from sqlite3 import Error
 
 def get_network():
-    network = []
+    network_list = []
     try:
         db_file = r"./db/MMM-SQLite.db"
         conn = sqlite3.connect(db_file)
         cur = conn.cursor()
         res = cur.execute("SELECT * FROM network")
-        network = res.fetchall()
+        networks = res.fetchall()
+        for i in networks:
+            network = {
+                "network_id": i[0],
+                "poll_id": i[1],
+                "network_interface": i[2],
+                "network_status": i[3],
+                "network_speed": i[4]
+            }
+            network_list.append(network)
     except Error as e:
         print(e)
     finally:
         if conn:
             conn.close()
-    return network
+    return network_list
 
 def get_network_by_id(network_id):
     network = {}
