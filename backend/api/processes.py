2885ca2190cdafac175629b4f8d3fa82e65c668d
Caleb-Rogers <rogerscaleb2019@gmail.com>
2023-04-16 14:17:25 -0400

    Gets all processes from the most recent poll_id

--- backend/api/processes.py
+++ backend/api/processes.py
@@ -31,28 +31,30 @@ def get_processes():
     return process_list
 
 def get_latest_process():
-    latest_process = {}
+    process_list = []
     try:
         db_file = r"./db/MMM-SQLite.db"
         conn = sqlite3.connect(db_file)
         cur = conn.cursor()
-        res = cur.execute("SELECT * FROM processes ORDER BY poll_id DESC LIMIT 1")
-        latest_process = res.fetchone()
-        latest_process = {
-                    "poll_id": latest_process[0],
-                    "process_id": latest_process[1],
-                    "process_name": latest_process[2],
-                    "process_status": latest_process[3],
-                    "cpu_percent": latest_process[4],
-                    "num_thread": latest_process[5],
-                    "memory_usage": latest_process[6]
-                }
+        res = cur.execute("SELECT * FROM processes WHERE poll_id = (SELECT MAX(poll_id) FROM processes) ORDER BY process_id ASC")
+        processes = res.fetchall()
+        for i in processes:
+            latest_process = {
+                "poll_id": i[0],
+                "process_id": i[1],
+                "process_name": i[2],
+                "process_status": i[3],
+                "cpu_percent": i[4],
+                "num_thread": i[5],
+                "memory_usage": i[6]
+            }
+            process_list.append(latest_process)
     except Error as e:
         print(e)
     finally:
         if conn:
             conn.close()
-    return latest_process
+    return process_list
 
 def get_latest_process_by_process_id(process_id):
     db_file = r"./db/MMM-SQLite.db"
